0b3242eca2c1e0abc74ae96e51b2af4a7cd0ab4c
huim_lin <huim_lin@haihangyun.com>
2017-01-09 17:26:02 +0800

    add

--- app/src/main/java/com/huim/money/activity/ShoppingList.java
+++ app/src/main/java/com/huim/money/activity/ShoppingList.java
@@ -1,7 +1,8 @@
 package com.huim.money.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import com.huim.money.R;
 
@@ -11,5 +12,10 @@ public class ShoppingList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_shopping_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
