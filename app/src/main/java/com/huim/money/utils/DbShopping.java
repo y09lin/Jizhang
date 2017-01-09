652e67bbdbe36cc8b2555bdf85aa37b72d352fb0
huim_lin <huim_lin@haihangyun.com>
2017-01-09 14:54:22 +0800

    DB

--- app/src/main/java/com/huim/money/utils/DbShopping.java
+++ app/src/main/java/com/huim/money/utils/DbShopping.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import com.huim.money.bean.Shopping;
 
 import org.xutils.DbManager;
+import org.xutils.ex.DbException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 购物数据存储
@@ -13,7 +17,27 @@ import org.xutils.DbManager;
 public class DbShopping {
     public static boolean saveItem(Context context, Shopping shopping){
         DbManager dbManager=DBManagerFactory.getIns(context).getDb();
-        if (dbManager!=null){}
+        if (dbManager!=null && shopping!=null){
+            try {
+                dbManager.saveOrUpdate(shopping);
+                return true;
+            } catch (DbException e) {
+                e.printStackTrace();
+            }
+        }
         return false;
+    }
+
+    public static List<Shopping> getShoppings(Context context){
+        List<Shopping> list=new ArrayList<>();
+        DbManager dbManager=DBManagerFactory.getIns(context).getDb();
+        if (dbManager!=null){
+            try {
+                list=dbManager.findAll(Shopping.class);
+            } catch (DbException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
